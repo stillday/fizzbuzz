e6100418a03e01f634b879166de51de460756371
Jan Drenkhahn <jan-dennisd@outlook.at>
2016-09-14 09:24:08 +0200

    utf8 eingebaut, zahlen überprüfung eingebaut, kleine verbesserungen

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,21 +1,26 @@
-print "Willkommen zu FizzBuzz, dem neuen Spiele Spass!"
+# -*- coding: utf-8 -*-
+print "Willkommen zu FizzBuzz, dem neuen Spiele Spaß!"
 
 while True:
     try:
-        number = int(raw_input("Waehle eine Zahl zwischen 1 und 100: "))
+        number = int(raw_input("Wähle eine Zahl zwischen 1 und 100: "))
+
+        while number < 1 or number > 100:
+
+            number= int(raw_input("Die Zahl muss zwischen 1 und 100 liegen: "))
 
         for x in range(1,number+1):
 
             if x % 3 == 0 and x % 5 == 0:
-                print "fizzbuzz"
+                print "FizzBuzz"
 
             elif x % 3 == 0:
-                print "fizz"
+                print "Fizz"
 
             elif x % 5 == 0:
-                print "buzz"
+                print "Buzz"
             else:
                 print x
-        print "Neue runde, neues Glueck :)"
+        print "Neues Spiel, neues Glück :)"
     except ValueError:
         print "Bitte nur Zahlen eingeben"
